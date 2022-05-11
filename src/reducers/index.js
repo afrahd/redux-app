af02f44308640ccc6ba1a40a762e658ed363be6f
Afra Dawood <you@example.com>
2022-05-11 15:47:00 +0400

    redux app initialized

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import countReducer from "./counter";
+import loggedReducer from "./isLogged";
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers({
+    counter: countReducer,
+    isLogged: loggedReducer
+    
+})
+
+export default allReducers;
